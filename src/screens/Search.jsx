976d2626e211a996ca843c4f8f782d092e3e9d4d
Zack-Diehl <95538980+Zack-Diehl@users.noreply.github.com>
2023-08-05 02:21:25 -0400

    Attempt to display posts from database in search
    - updated imports as needed
    - useEffect to update setPosts to be an array of posts
    - couldn't get it displayed, "Property 'Text' doesn't exist" among other errors I would get

--- src/screens/Search.jsx
+++ src/screens/Search.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import {StyleSheet, View} from 'react-native';
 import Carousel from 'react-native-snap-carousel';
+import  { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../firebase';
+
+const emotions = ["Alone", "Angry", "Anniversary", "Attitude", "Awesome", "Awkward Moment", "Beard", "Beautiful", "Best", "Bike", "Birthday", "Break Up", "Brother", "Busy"] 
 
 export default function Search() {
+  const [posts, setPosts] = useState([]);
+  const postsCollectionRef = collection(db, 'posts');
+  selectedEmotion = 'Anniversary'
+
+  useEffect(() => {
+    const getPosts = async () => {
+    const data = await getDocs(postsCollectionRef);
+    setPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id})));
+  };
+
+    getPosts();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Carousel layout={'tinder'} layoutCardOffset={9} />
+    <View>
+      {posts.map((post) => {
+        return <div> 
+          <Text> Quote: {post.text} </Text>
+          <Text> {post.selectedEmotion} </Text>
+        </div>;
+      })}
     </View>
+
+
+    // <View style={styles.container}>
+    //   <Carousel layout={'tinder'} layoutCardOffset={9} />
+    // </View>
   );
 }
 
